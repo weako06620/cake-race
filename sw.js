3ea790dfedd93b963babf25169266f19d5cfdbed
weako06620 <weako06800@gmail.com>
2026-04-01 14:53:42 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,5 @@
-const CACHE_NAME = 'jeu-debile-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-// Install: cache core assets
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-// Activate: cleanup old caches
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Fetch: network first, fallback to cache
-self.addEventListener('fetch', e => {
-  // Skip non-GET and cross-origin PeerJS signaling
-  if (e.request.method !== 'GET') return;
-  
-  e.respondWith(
-    fetch(e.request)
-      .then(response => {
-        // Cache successful responses
-        if (response.ok) {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        }
-        return response;
-      })
-      .catch(() => {
-        // Offline: serve from cache
-        return caches.match(e.request).then(cached => {
-          return cached || new Response('Offline', { status: 503 });
-        });
-      })
-  );
-});
+var CACHE='jd-v4';
+var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(k){return Promise.all(k.filter(function(n){return n!==CACHE}).map(function(n){return caches.delete(n)}))}).then(function(){return self.clients.claim()}))});
+self.addEventListener('fetch',function(e){if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(function(r){if(r.ok){var c=r.clone();caches.open(CACHE).then(function(cache){cache.put(e.request,c)})}return r}).catch(function(){return caches.match(e.request).then(function(c){return c||new Response('Offline',{status:503})})}))});
